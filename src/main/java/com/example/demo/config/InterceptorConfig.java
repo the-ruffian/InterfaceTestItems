61b65afa81bb9fd003e1c450310020ecddcf8d7f
the-ruffian <13558622779@163.com>
2022-05-27 00:40:07 +0800

    feat: JWT

---
+++ src/main/java/com/example/demo/config/InterceptorConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import com.example.demo.insterceptors.JWTInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 接口拦截配置
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JWTInterceptor())
+                //token验证接口
+                .addPathPatterns("/api/**")
+                //放行接口
+                .excludePathPatterns("/api/user/login","/api/user/register","/api/getCode","/api/forgetPassword",
+                        "/api/columnMenu/list", "/api/article/list","/api/article/body",
+                        "/swagger-ui.html/**", "/v2/**","/swagger-resources/**","/webjars/**","/*.html");
+    }
+}

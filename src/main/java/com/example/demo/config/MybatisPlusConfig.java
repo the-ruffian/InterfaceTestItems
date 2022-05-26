53a8b00640154ec5eda53cc744a90ca61ae590c4
the-ruffian <13558622779@163.com>
2022-05-27 00:41:41 +0800

    feat: MP_conf

---
+++ src/main/java/com/example/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * MP拦截器
+ */
+@Configuration
+@MapperScan("com.example.demo.mapper")
+public class MybatisPlusConfig {
+    /*
+     * 分页拦截器
+     * */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+}

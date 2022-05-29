6e27f6484290bde0e7260343f35a7a5046ab8f02
bugpz <13558622779@163.com>
2022-05-29 13:44:19 +0800

    feat：User table mapping

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,47 @@
+/*
+ * @Description:User
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date: 2022-05-29 12:54
+ * @LastEditTime: 2022-05-29 12:54
+ * @LastEditors: the-ruffian
+ */
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+/**
+ * @author the-ruffian
+ */
+
+@Data
+@TableName(value = "user")
+@ApiModel(value = "用户表")
+public class User {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    @ApiModelProperty(value = "用户密码")
+    private String password;
+    @ApiModelProperty(value = "手机号")
+    private String phone;
+    @ApiModelProperty(value = "性别")
+    private Integer gender;
+    @ApiModelProperty(value = "备注")
+    private String remark;
+    @ApiModelProperty(value = "身份证号")
+    private String idNumber;
+    private Timestamp createTime;
+    private Timestamp updateTime;
+    private Timestamp loginTime;
+    @ApiModelProperty(value = "用户名")
+    private String username;
+    @ApiModelProperty(value = "邮箱")
+    private String email;
+}

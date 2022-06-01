5a55df6fb11e3ea9c4989365e9a5106c49e81e9f
bugpz <13558622779@163.com>
2022-06-01 10:39:37 +0800

    feat：注册接口

--- src/main/java/com/example/demo/services/user/server/UserService.java
+++ src/main/java/com/example/demo/services/user/server/UserService.java
@@ -1,4 +1,8 @@
 package com.example.demo.services.user.server;
 
-public class UserService {
+import com.example.demo.model.dto.RegisterDto;
+import com.example.demo.utils.model.OpenResponse;
+
+public interface UserService {
+    OpenResponse register(RegisterDto registerDto);
 }

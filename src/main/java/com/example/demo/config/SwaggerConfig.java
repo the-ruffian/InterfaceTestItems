50e8ad146debb267d209b7c43ed4960acfdc4693
bugpz <13558622779@163.com>
2022-06-01 11:26:13 +0800

    feat：swagger配置

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author bugpz
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket getApiInfo() {
+        return new Docket(DocumentationType.SWAGGER_2)
+//            .groupName("outer api")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}

8e538ef1dcf35dd4a7070797c3bd134b8a5851e6
bugpz <13558622779@163.com>
2022-06-02 09:05:54 +0800

    docs：增加注释

--- src/main/java/com/example/demo/services/user/server/UserService.java
+++ src/main/java/com/example/demo/services/user/server/UserService.java
@@ -3,6 +3,14 @@ package com.example.demo.services.user.server;
 import com.example.demo.model.dto.RegisterDto;
 import com.example.demo.utils.model.OpenResponse;
 
+/**
+ * @author the-ruffian
+ */
 public interface UserService {
-    OpenResponse register(RegisterDto registerDto);
+    /**
+     *  注册接口
+     * @param registerDto RegisterDto
+     * @return 注册信息
+     */
+    OpenResponse<Object> register(RegisterDto registerDto);
 }

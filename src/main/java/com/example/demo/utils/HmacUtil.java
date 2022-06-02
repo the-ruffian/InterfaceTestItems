e2b29f9e12164d7f5e11fd87d4d7aa603d948f6b
bugpz <13558622779@163.com>
2022-06-02 13:03:07 +0800

    feat：封装加密类

---
+++ src/main/java/com/example/demo/utils/HmacUtil.java
@@ -0,0 +1,27 @@
+package com.example.demo.utils;
+
+
+import org.bouncycastle.util.encoders.Hex;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author the-ruffian
+ */
+public class HmacUtil {
+    public static String getSsa256Str(String str){
+        MessageDigest messageDigest;
+        String encodeStr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = messageDigest.digest(str.getBytes(StandardCharsets.UTF_8));
+            encodeStr = Hex.toHexString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encodeStr;
+    }
+}

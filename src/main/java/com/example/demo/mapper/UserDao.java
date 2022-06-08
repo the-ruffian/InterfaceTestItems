241f77bc0ea2cf98691e348c760b07c4ab3e0ff6
bugpz <13558622779@163.com>
2022-06-08 13:39:57 +0800

    fix：新增修改个人信息sql

--- src/main/java/com/example/demo/mapper/UserDao.java
+++ src/main/java/com/example/demo/mapper/UserDao.java
@@ -2,6 +2,9 @@ package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.entity.User;
+import com.example.demo.model.dto.UpdateDto;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,5 +13,18 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public interface UserDao extends BaseMapper<User> {
-
+    /**
+     * 修改个人信息
+     * @param updateDto phone e_mali gender username
+     */
+    @Update("<script>" +
+            "update " +
+            "user " +
+            "set phone=CONCAT(#{updateDto.phone})" +
+            "<if test=\"updateDto.gender!=null\">, gender=CONCAT(#{updateDto.gender})</if>" +
+            "<if test=\"updateDto.username!=null\">, username=CONCAT(#{updateDto.username})</if>" +
+            "<if test=\"updateDto.email!=null\">, email=CONCAT(#{updateDto.email})</if>" +
+            " where phone=#{updateDto.phone}" +
+            "</script>")
+    void userUpdateSelf(@Param("updateDto")UpdateDto updateDto);
 }
